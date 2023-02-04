ee9d7c4904c55dab6f91ba80fa80ce8451780d35
Kye <kyehorbury@msn.com>
2023-02-04 14:49:31 +0800

    BossFixes

--- Assets/Scripts/DamageDealer.cs
+++ Assets/Scripts/DamageDealer.cs
@@ -4,13 +4,13 @@ public class DamageDealer : MonoBehaviour
 {
     public int m_health = 100;
     public int damage = 5;
-    LayerMask mask;
+    public LayerMask mask;
     [SerializeField] private float _knockbackRadius = 0.5f; 
     private bool _playerInKnockback;
     public CharacterController2D player;
     public GameObject m_Bones;
     private void Start() {
-        mask = LayerMask.GetMask("Player");
+        //mask = LayerMask.GetMask("Player");
     }
 
     private void Update()
@@ -28,7 +28,7 @@ public class DamageDealer : MonoBehaviour
 
         if(mask != 0)
         {        
-        Collider2D checkRadius = Physics2D.OverlapCircle(transform.position, _knockbackRadius, mask);
+        Collider2D checkRadius = Physics2D.OverlapCircle(transform.position, _knockbackRadius);
             if(checkRadius)
             {
                 //fix this... it goes the wrong way. 
